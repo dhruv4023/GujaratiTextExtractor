e46be2302991b629120e45307bf5f9668757f69a
dhruv <dhruv20345@gmail.com>
2023-09-11 21:11:45 +0530

    extractor function created for clean image

---
+++ Extractor.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import pytesseract
+# Specify the Tesseract executable path (update this with your installation path)
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Apps\\Tesseract-OCR\\tesseract.exe'
+
+
+def extract_text(img_file:str):
+    # Open the image
+    image = Image.open(img_file)
+
+    # Use pytesseract to extract text
+    extracted_text = pytesseract.image_to_string(image, lang='guj', output_type=pytesseract.Output.STRING)
+
+    # Print or use the extracted Gujarati text
+    return (extracted_text)
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from Extractor import extract_text
+from WriteToTXTfile import write_to_txt_file 
+
+print(write_to_txt_file(extract_text("TestImgs\\test2.png")))
