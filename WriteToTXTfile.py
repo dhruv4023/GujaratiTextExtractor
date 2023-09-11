6330b210f654b7e2e583e40613389738ecd34511
dhruv <dhruv20345@gmail.com>
2023-09-11 21:10:21 +0530

    create and save data to txt file fun created

---
+++ WriteToTXTfile.py
@@ -0,0 +1,25 @@
+import datetime
+import os
+
+
+def write_to_txt_file(data):
+    # Name of the output folder
+    output_folder = 'outputs'
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    currrent_date_time=(str(datetime.datetime.now())[:19].replace(" ","_").replace(":","_"))
+    file_name=currrent_date_time+".txt"
+    
+    # Specify the file path inside the output folder
+    output_file_path = os.path.join(output_folder, file_name)
+    # Open a .txt file in write mode
+    with open(output_file_path, 'w') as file:
+        # Write data to the file
+        file.write(data)
+    # The file will be automatically closed when the 'with' block exits
+    return  "data written at: "+'/output/'+currrent_date_time+'.txt'
+
+write_to_txt_file("hello")
