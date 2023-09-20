23bf4f18d542c2ce11d8072987f95d5feb780782
dhruv <dhruv20345@gmail.com>
2023-09-20 23:00:13 +0530

    tst

--- MyFunctions/WriteToTXTfile.py
+++ MyFunctions/WriteToTXTfile.py
@@ -1,24 +1,24 @@
-import datetime
-import os
-import time
+# import datetime
+# import os
+# import time
 
 
-def write_to_txt_file(data):
-    # Name of the output folder
-    output_folder = 'outputs'
-    time.sleep(1)
-    # Create the output folder if it doesn't exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+# def write_to_txt_file(data):
+#     # Name of the output folder
+#     output_folder = 'outputs'
+#     time.sleep(1)
+#     # Create the output folder if it doesn't exist
+#     if not os.path.exists(output_folder):
+#         os.makedirs(output_folder)
 
-    currrent_date_time=(str(datetime.datetime.now())[:19].replace(" ","_").replace(":","_"))
-    file_name=currrent_date_time+".txt"
+#     currrent_date_time=(str(datetime.datetime.now())[:19].replace(" ","_").replace(":","_"))
+#     file_name=currrent_date_time+".txt"
     
-    # Specify the file path inside the output folder
-    output_file_path = os.path.join(output_folder, file_name)
-    # Open a .txt file in write mode
-    with open(output_file_path, 'w') as file:
-        # Write data to the file
-        file.write(data)
-    # The file will be automatically closed when the 'with' block exits
-    return  "data written at: "+'/output/'+currrent_date_time+'.txt'
+#     # Specify the file path inside the output folder
+#     output_file_path = os.path.join(output_folder, file_name)
+#     # Open a .txt file in write mode
+#     with open(output_file_path, 'w') as file:
+#         # Write data to the file
+#         file.write(data)
+#     # The file will be automatically closed when the 'with' block exits
+#     return  "data written at: "+'/output/'+currrent_date_time+'.txt'
