ffdc842270b52e2d2a1f3cd0b74c1900b041aec8
dhruv <dhruv20345@gmail.com>
2023-10-25 16:12:55 +0530

    251023

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from MyFunctions.WriteToTXTfile import write_to_txt_file
 from MyFunctions.Gary_scale import gray_Image 
 from MyFunctions.Rm_Noice import rm_noice
 from MyFunctions.CleanTxtVisible import cleanTxtVisible 
+from MyFunctions.Shawdow import rm_shadow 
 # for i in range(1,6):
 # print(write_to_txt_file(extract_text(img_file="TestImgs\\Gujarati_Image_Test_"+str(5)+".jpg")))
 # print(write_to_txt_file(extract_text(img_file="TestImgs\\test3.png")))
 
 # img_path="TestImgs\\en_tst_2.png"
-img_path="TestImgs\\tst_dev.jpeg"
 # img_path="temp//shadows_out_norm.jpg"
 # img_path="cleaned_image.jpg"
 # img_path="temp/image_with_border.jpg"
@@ -21,7 +21,13 @@ img_path="TestImgs\\tst_dev.jpeg"
 # cleaned_img_path=clean_img(img_path=img_path)
 # img_path="temp\\binary.jpg"
 
+img_path="TestImgs\\tst_dev.jpeg"
+
+# img_path="TestImgs\\Gujarati_Image_Test_5.jpg"
+# img_path=rm_shadow(img_path) 
+# img_path=rm_noice(img_path) 
+# img_path=thin_font(img_path) 
 img_path=gray_Image(img_path) 
 # img_path=cleanTxtVisible(img_path) 
-# img_path=thin_font(img_path) 
+# img_path="image.png"
 print(write_to_txt_file(extract_text(img_file=img_path,lang="eng"))) # eng -> english  # guj -> gujarati
---
+++ tst.py
@@ -0,0 +1,52 @@
+# import cv2
+
+# import numpy as np
+# def nothing(x):
+#     pass
+
+# def dark_txt(img_path):
+#     print(img_path)
+#     # Load image
+#     image = cv2.imread(img_path)
+
+#     # Create trackbars for color change
+#     # Hue is from 0-179 for Opencv
+#     # cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+#     # cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+#     # cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+#     # cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+#     # cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+#     # cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+#     # Set default value for Max HSV trackbars
+#     # cv2.setTrackbarPos('HMax', 'image', 179)
+#     # cv2.setTrackbarPos('SMax', 'image', 255)
+#     # cv2.setTrackbarPos('VMax', 'image', 255)
+
+#     # Initialize HSV min/max values
+#     hMin = sMin = vMin = hMax = sMax = vMax = 0
+#     phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+#     # while(1):
+#     # Get current positions of all trackbars
+#     hMin= 0 #= cv2.getTrackbarPos('HMin', 'image')
+#     sMin= 0 #= cv2.getTrackbarPos('SMin', 'image')
+#     vMin= 196 #= cv2.getTrackbarPos('VMin', 'image')
+#     hMax= 72 #= cv2.getTrackbarPos('HMax', 'image')
+#     sMax= 153 #= cv2.getTrackbarPos('SMax', 'image')
+#     vMax= 255 #= cv2.getTrackbarPos('VMax', 'image')
+
+#     # Set minimum and maximum HSV values to display
+#     lower = np.array([hMin, sMin, vMin])
+#     upper = np.array([hMax, sMax, vMax])
+
+#     # Convert to HSV format and color threshold
+#     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#     mask = cv2.inRange(hsv, lower, upper)
+#     result = cv2.bitwise_and(image, image, mask=mask)
+
+#     # Display result image
+#     img_path="temp//dark_txt.png"
+#     cv2.imwrite(img_path, result)
+#     return img_path
+# dark_txt("temp//shadows_out_x.jpg")
